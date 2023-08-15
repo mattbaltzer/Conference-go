edc740cbf51335b66df47d32a3a3326dc1ece24c
Matt Baltzer <baltzer.matt@gmail.com>
2023-08-14 17:42:58 -0700

    Moved my files around, created a monolith and the microservices

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -1,11 +1,10 @@
 import json
 from django.http import JsonResponse
 from common.json import ModelEncoder
-from .models import Attendee
-from events.models import Conference
-from events.api_views import ConferenceListEncoder
+from .models import Attendee, ConferenceVO
+# from events.models import Conference
+# from events.api_views import ConferenceListEncoder
 from django.views.decorators.http import require_http_methods
-
 
 
 class AttendeeListEncoder(ModelEncoder):
@@ -15,10 +14,14 @@ class AttendeeListEncoder(ModelEncoder):
     ]
 
 
+class ConferenceVODetailEncoder(ModelEncoder):
+    model = ConferenceVO
+    properties = ["name", "import_href"]
+
 @require_http_methods(["GET", "POST"])
-def api_list_attendees(request, conference_id):
+def api_list_attendees(request, conference_vo_id=None):
     if request.method == "GET":
-        attendees = Attendee.objects.filter(conference=conference_id)
+        attendees = Attendee.objects.filter(conference=conference_vo_id)
         return JsonResponse(
             {"attendees": attendees},
             encoder=AttendeeListEncoder
@@ -26,9 +29,10 @@ def api_list_attendees(request, conference_id):
     else:
         content = json.loads(request.body)
         try:
-            conference = Conference.objects.get(id=conference_id)
+            conference_href = f'/api/conferences/{conference_vo_id}/'
+            conference = ConferenceVO.objects.get(import_href=conference_href)
             content["conference"] = conference
-        except Conference.DoesNotExist:
+        except ConferenceVO.DoesNotExist:
             return JsonResponse(
                 {"message": "Invalid conference id"},
                 status=400,
@@ -51,7 +55,7 @@ class AttendeeDetailEncoder(ModelEncoder):
         "conference",
     ]
     encoders = {
-        "conference": ConferenceListEncoder()
+        "conference": ConferenceVODetailEncoder()
     }
 
 
